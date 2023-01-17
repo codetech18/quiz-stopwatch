22d32bde385432065a596839dd43af366f231abc
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-01-17 19:03:28 +0100

    reset and stop errors rectified

--- stopwatch.js
+++ stopwatch.js
@@ -3,8 +3,8 @@ let tens = 00;
 let getSeconds = document.querySelector('.seconds');
 let getTens = document.querySelector('.tens');
 let btnStart = document.querySelector('.btn-start');
-let btnStop = document.querySelector('.btn-Stop');
-let btnReset = document.querySelector('.btn-Reset');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
 let interval;
 
 
